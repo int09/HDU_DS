17488434e50a612fa50490d0f86e2179344886c6
Jack <1813834555@qq.com>
2015-11-23 22:07:53 +0800

    Fix bug

--- 2011/Five-2.cc
+++ 2011/Five-2.cc
@@ -31,3 +31,29 @@ void A8(BiTree T) {
 }
 
 
+void A8(BiTree T) {
+	int front = 0, rear = 0;
+	BTNode *que[MaxSize], *q;
+	if (p != NULL) {
+		rear = (rear + 1) % MaxSize;
+		que[rear] = p;
+		int depth = 1;
+		while (front != rear) {
+			front = (front + 1) % MaxSize;
+			q = que[front];
+			if (q->lchild != NULL) {
+				rear = (rear + 1) % MaxSize;
+				que[rear] = q->lchild;
+			}
+			if (q->rchild != NULL) {
+				rear = (rear + 1) % MaxSize;
+				que[rear] = q->rchild;
+			}
+			if (front == high) {
+				high = rear;
+				depth++;
+			}
+		}
+	}
+	cout << "The depth is : " << depth[front] << endl;
+}
--- 2013/Six-1.cc
+++ 2013/Six-1.cc
@@ -4,8 +4,9 @@ typedef struct LNode {
 } LNode, *LinkList;
 
 void Diff(LinkList &L1, LinkList L2) {
-	LNode *p = L1->next, *q = L2->next, pre = L1;
+	LNode *p = L1->next, pre = L1;
 	while (p != NULL) {
+		LNode *q = L2->next;
 		while (q != NULL) {
 			if (p->data == q->data) {
 				pre->next = p->next;
